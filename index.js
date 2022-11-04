1e1af2d44bd1d3f3e1430fcee820fc4a7d933edf
sofyao4 <113201319+sofyao4@users.noreply.github.com>
2022-11-04 13:15:46 -0400

    11/4

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // select elements by id or class
+const DOMSelectors = {
+  button: document.getElementById("btn"),
+  box: document.getElementById("container-box"),
+  input: document.querySelector(`#input`),
+};
+
+DOMSelectors.box.i;
