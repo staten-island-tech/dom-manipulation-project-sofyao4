82c6dd1fd281f0fb8e2ff1a351da07cb596c121d
sofyao4 <113201319+sofyao4@users.noreply.github.com>
2022-11-21 21:23:28 -0500

    final

--- index.js
+++ index.js
@@ -9,18 +9,20 @@ function getResult() {
   DOMSelectors.result.addEventListener("click", function (event) {
     let name = DOMSelectors.name.value;
     event.preventDefault();
+    DOMSelectors.name.value = "";
     DOMSelectors.box.insertAdjacentHTML(
       "beforeend",
       `<p>Hi! My name is ${name}. </p>`
     );
-    DOMSelectors.name.value = "";
-
-    DOMSelectors.removebtn.addEventListener("click", function () {
-      //const html = DOMSelectors.box.html;
-      //html.parentNode.removeChild(html);
-      const text = DOMSelectors.box;
-      text.parentNode.removeChild(text);
+    const remove = document.querySelectorAll("removebtn");
+    remove.forEach((remove) => {
+      remove.addEventListener("click", function () {
+        this.parentElement.remove();
+      });
     });
   });
 }
+DOMSelectors.removebtn.addEventListener("click", function () {
+  DOMSelectors.box.innerHTML = "";
+});
 getResult();
